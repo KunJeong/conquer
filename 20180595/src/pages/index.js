29a49a19cb7b088faacd69656b7193cd926c99e4
KunJeong <kunjeong99@naver.com>
2021-05-20 05:47:51 +0900

    [UI] added margins and changes spacing, font size

--- 20180595/src/pages/index.js
+++ 20180595/src/pages/index.js
@@ -9,145 +9,114 @@ import {
   Button,
 } from "@material-ui/core";
 import { makeStyles } from "@material-ui/core/styles";
-import Isometric from "../components/Isometric";
+import Map from "../components/Map";
 import axios from "axios";
 import { observer } from "mobx-react-lite";
 import { useStores } from "../hooks/useStores";
 import { useEffect } from "react";
-// import useSWR from 'swr'
 
 const useStyles = makeStyles({
-  // bullet: {
-  //   display: 'inline-block',
-  //   margin: '0 2px',
-  //   transform: 'scale(0.8)',
-  // },
   title: (props) => ({
     ...props.style,
-    fontSize: 24,
+    fontSize: 18,
   }),
   body: (props) => ({
     ...props.style,
     fontSize: 20,
   }),
-  // pos: {
-  //   marginBottom: 12,
-  // },
+  seconds: (props) => ({
+    ...props.style,
+    fontSize: 30,
+  }),
   paper: {
     padding: "20px",
     textAlign: "center",
   },
 });
 
-// const getter = url => axios.get(url).then(res => res.data)
+function AddTodo(props) {
+  const classes = useStyles();
 
-// function AddTodo(props) {
-//   const classes = useStyles();
+  const [title, setTitle] = React.useState("");
+  const editTitle = (event) => {
+    setTitle(event.target.value);
+  };
 
-//   const [title, setTitle] = React.useState("")
-//   const editTitle = (event) => {
-//     setTitle(event.target.value)
-//   }
-
-//   return (
-//     <Paper className={classes.paper} elevation={3} margin={4}>
-//       <Box>
-//         <TextField
-//           label="Title"
-//           value={title}
-//           onChange={editTitle}
-//           margin="normal"
-
-//         ></TextField>
-//         <Button onClick={props.addTodo(title)}>Create</Button>
-//       </Box>
-//     </Paper>
-//   )
-// }
+  return (
+    <Paper className={classes.paper} elevation={3} margin={4}>
+      <Box>
+        <TextField
+          label="Title"
+          value={title}
+          onChange={editTitle}
+          margin="normal"
+        ></TextField>
+        <Button onClick={props.addTodo(title)}>Create</Button>
+      </Box>
+    </Paper>
+  );
+}
 
 const Index = observer(function Index() {
   const classes = useStyles();
   const { cells, ui } = useStores();
-  // const [todos, setTodos] = React.useState([])
-  // const [addingTodo, setAddingTodo] = React.useState(false)
-  // const [selectedI, setSelectedI] = React.useState(0)
-  // const [selectedJ, setSelectedJ] = React.useState(0)
-
   const seconds = new Date(ui.secondsRemaining * 1000)
     .toISOString()
     .substr(11, 8);
 
   useEffect(() => {
-    // cells.initStore()
     cells.getCells();
   }, []);
-  // const addTodo = (i, j) => {
-  //   // const newTodos = todos.push({i, j, title: "something"})
-  //   // setTodos(newTodos)
-  //   setAddingTodo(true);
-  //   setSelectedI(i);
-  //   setSelectedJ(j);
-  // }
 
-  // const createTodo = (title) => () => {
-  //   console.log(`title: ${title}, i: ${selectedI}, j: ${selectedJ}`)
-
-  //   axios.post('http://localhost:3000/todos', {
-  //     title: title,
-  //     i: selectedI,
-  //     j: selectedJ
-  //   }).then(function (response) {
-
-  //   })
-  // }
   return (
-    <Grid container spacing={2}>
-      <Grid item xs={4}>
-        {ui.timerMode ? (
-          <Paper className={classes.paper} elevation={3} margin={4}>
-            <Box>
-              <Typography
-                className={classes.title}
-                color="textSecondary"
-                gutterBottom
-              >
-                Focus on a task for {ui.secondsTotal} seconds!
-              </Typography>
-              <Typography
-                className={classes.seconds}
-                color="textSecondary"
-                gutterBottom
-              >
-                {seconds}
-              </Typography>
-            </Box>
-          </Paper>
-        ) : (
-          <Paper className={classes.paper} elevation={3} margin={4}>
-            <Box>
-              <Typography
-                className={classes.title}
-                color="textSecondary"
-                gutterBottom
-              >
-                no selection
-              </Typography>
-            </Box>
-          </Paper>
-        )}
-        <Button
-          onClick={() => {
-            cells.initStore();
-          }}
-        >
-          {" "}
-          Reset{" "}
-        </Button>
+    <Box mx={2}>
+      <Grid container spacing={6}>
+        <Grid item xs={4}>
+          {ui.timerMode ? (
+            <Paper className={classes.paper} elevation={3} margin={4}>
+              <Box>
+                <Typography
+                  className={classes.title}
+                  color="textSecondary"
+                  gutterBottom
+                >
+                  Focus on a task for {ui.secondsTotal} seconds!
+                </Typography>
+                <Typography
+                  className={classes.seconds}
+                  color="textSecondary"
+                  gutterBottom
+                >
+                  {seconds}
+                </Typography>
+              </Box>
+            </Paper>
+          ) : (
+            <Paper className={classes.paper} elevation={3} margin={4}>
+              <Box>
+                <Typography
+                  className={classes.title}
+                  color="textSecondary"
+                  gutterBottom
+                >
+                  {`${cells.cells[ui.selection].name}`}
+                </Typography>
+              </Box>
+            </Paper>
+          )}
+          <Button
+            onClick={() => {
+              cells.initStore();
+            }}
+          >
+            Reset
+          </Button>
+        </Grid>
+        <Grid item xs={8}>
+          <Map />
+        </Grid>
       </Grid>
-      <Grid item xs={6}>
-        <Isometric />
-      </Grid>
-    </Grid>
+    </Box>
   );
 });
 
