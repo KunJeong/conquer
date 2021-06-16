aab4b571d16de3f1de0cbe187af36bd390a6d1aa
KunJeong <kunjeong99@naver.com>
2021-06-16 09:08:55 +0900

    [bug] fixed conflict

--- 20180595/src/components/IsometricGrid.tsx
+++ 20180595/src/components/IsometricGrid.tsx
@@ -1,7 +1,6 @@
 //@ts-check
 import CellView from "./CellView";
 import { observer } from "mobx-react-lite";
-<<<<<<< HEAD
 import { Box } from "@material-ui/core";
 import { useStores } from "../hooks";
 import React, { Fragment } from "react";
@@ -11,33 +10,6 @@ interface IsometricGridProps {
   cells: Cell[];
   [rest: string]: any;
 }
-=======
-import { useStores } from "../hooks/useStores";
-import React from "react";
-
-const sqrt3 = 1.73205;
-
-const IsometricGrid = observer(function _IsometricGrid(props) {
-  const { cells, ui } = useStores();
-  const animationRef = React.useRef(null);
-
-  const halfSpan = Math.max(
-    Math.floor(props.width / props.childWidth),
-    Math.floor((props.height * sqrt3) / props.childWidth)
-  );
-  const totalSpan = halfSpan * 2 + 1;
-  console.log(`width: ${props.width}, child: ${props.childWidth}`);
-
-  console.log(`half: ${halfSpan}, total: ${totalSpan}`);
-
-  const { minI, minJ, maxI, maxJ } = cells.mapSize;
-  const iSize = maxI - minI + 1;
-  const jSize = maxJ - minJ + 1;
-  console.log(`size: ${iSize}, ${jSize}`);
-  const onClick = (index, i, j) => {
-    // if (!ui.isPanning) {
-    console.log("clicked");
->>>>>>> master
 
 const IsometricGrid = observer(function _IsometricGrid({
   cells,
@@ -70,21 +42,5 @@ const IsometricGrid = observer(function _IsometricGrid({
     </Box>
   );
 });
-<<<<<<< HEAD
-=======
-
-// _IsometricGrid.defaultProps = {
-//   width: 500,
-//   height: 400,
-//   childwidth: 80,
-//   spacing: 100,
-//   cells: [
-//     { _id: "1", i: 0, j: 0 },
-//     { _id: "2", i: 1, j: 0 },
-//     { _id: "3", i: 0, j: 1 },
-//     { _id: "4", i: 1, j: 1 },
-//   ],
-// };
->>>>>>> master
 
 export default IsometricGrid;
