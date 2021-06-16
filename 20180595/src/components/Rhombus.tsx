43ad672a12fbf4e38f9e6994349924fa2fb85ca1
KunJeong <kunjeong99@naver.com>
2021-06-16 09:11:01 +0900

    [bug] fixed conflict

--- 20180595/src/components/Rhombus.tsx
+++ 20180595/src/components/Rhombus.tsx
@@ -4,11 +4,7 @@ import { makeStyles } from "@material-ui/core/styles";
 import { mapDimensions } from "../constants";
 
 const useStyles = makeStyles({
-<<<<<<< HEAD
   rhombus: ({ width, ...props }: { width: number; [rest: string]: any }) => ({
-=======
-  rhombus: (props: RhombusProps) => ({
->>>>>>> master
     ...props.style,
     // backgroundColor: "#ffffff",
     // boxSizing: "border-box",
@@ -17,7 +13,6 @@ const useStyles = makeStyles({
     // padding: '5px',
     // willChange: "transform",
     left: "50%",
-<<<<<<< HEAD
     bottom: "0px",
     marginLeft: width * -0.5 * mapDimensions.sqrt1over2,
     marginBottom: `${
@@ -29,20 +24,6 @@ const useStyles = makeStyles({
     width: width * mapDimensions.sqrt1over2 + "px",
     height: width * mapDimensions.sqrt1over2 + "px",
     transform: `scale(1, ${mapDimensions.sqrt1over3}) rotate(45deg)`,
-=======
-    bottom: "50%",
-    // borderWidth: props.style.borderwidth * sqrt3,
-    marginLeft: props.width * sqrt1over2 * -0.5 + "px",
-    marginBottom: props.width * sqrt1over2 * -0.5 + "px",
-    width: props.width * sqrt1over2 + "px",
-    height: props.width * sqrt1over2 + "px",
-    transform: `scale(1, ${sqrt1over3}) rotate(45deg)`,
-    // borderRadius: '6px',
-
-    // props.borderWidth * sqrt3 * sqrt1over2 + 'px',
-
-    borderColor: "#000000",
->>>>>>> master
   }),
 });
 
@@ -51,19 +32,12 @@ interface RhombusProps {
   [rest: string]: any;
 }
 
-<<<<<<< HEAD
 function Rhombus(props: RhombusProps) {
   const classes = useStyles(props);
   return (
     <div {...props} className={`${classes.rhombus} ${props.className}`}>
       {props.children}
     </div>
-=======
-function Rhombus({ width = 120, ...props }: RhombusProps) {
-  const classes = useStyles({ width, ...props });
-  return (
-    <div className={`${classes.rhombus} ${props.className}`} {...props}></div>
->>>>>>> master
   );
 }
 
